5057bcdc76ca5ea6dee9289e8c9ddbe19f8e9868
shy <you@example.com>
2022-11-13 10:46:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+## 📦 Install
+
+Using npm:
+
+```bash
+npm install shy-utils
+```
+
+Using unpkg CDN:
+
+```html
+<script src="https://unpkg.com/shy-utils@0.0.1/dist/shy-utils.iife.js"></script>
+```
+
+## 🔨 Usage
+
+ESM:
+
+```javascript
+import { sum } from 'shy-utils';
+```
+
+Browser:
+
+```javascript
+ShyUtils.sum(1, 1);
+```
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'ShyUtils',
+            formats: ['es', 'umd', 'iife'],
+        },
+    },
+    test: {
+        /* for example, use global to avoid globals imports (describe, test, expect): */
+        // globals: true,
+    },
+});
